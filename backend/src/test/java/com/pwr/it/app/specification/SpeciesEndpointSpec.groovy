69ab598c1a10016c87b11af2e4f25b7d101d305e
wojciech.spoton@comarch.com <wojciech.spoton@comarch.com>
2020-04-18 12:39:13 +0200

    Get species list functionality

---
+++ backend/src/test/java/com/pwr/it/app/specification/SpeciesEndpointSpec.groovy
@@ -0,0 +1,60 @@
+package com.pwr.it.app.specification
+
+import com.jayway.jsonpath.JsonPath
+import com.pwr.it.app.data.seeder.DatabaseCleaner
+import com.pwr.it.app.data.seeder.SpeciesSeeder
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.inject.Inject
+
+@MicronautTest
+class SpeciesEndpointSpec extends Specification {
+
+    @Inject
+    SpeciesSeeder speciesSeeder
+
+    @Inject
+    @Shared
+    DatabaseCleaner databaseCleaner
+
+    @Inject
+    @Client('/')
+    RxHttpClient client
+
+    def setupSpec() {
+        cleanDatabase()
+        initSpecies()
+    }
+
+    @Unroll
+    def "endpoint return list of species"() {
+        setup:
+        def responseAsJson = client.toBlocking().retrieve(HttpRequest.GET('/api/species/all'), String)
+
+        expect:
+        JsonPath.read(responseAsJson, testedElement) == correctElement
+
+        where:
+        testedElement | correctElement
+        "[0]"         | "cat"
+        "[1]"         | "dog"
+        "[2]"         | "snake"
+    }
+
+    private cleanDatabase() {
+        databaseCleaner.clean()
+    }
+
+    private initSpecies() {
+        speciesSeeder.initCat()
+        speciesSeeder.initDog()
+        speciesSeeder.initSnake()
+    }
+
+}
--- backend/src/test/java/com/pwr/it/app/web/controller/AnimalControllerTest.java
+++ backend/src/test/java/com/pwr/it/app/web/controller/AnimalControllerTest.java
@@ -87,6 +87,7 @@ public class AnimalControllerTest {
                 .build();
     }
 
+
     @MockBean(AnimalService.class)
     AnimalService animalService() {
         return mock(AnimalService.class);
