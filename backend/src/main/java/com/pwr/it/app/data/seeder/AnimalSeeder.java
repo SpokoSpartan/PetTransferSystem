9c78aac889eb57cc56412afb8d87ad4bc75cf6e2
Piotr Kowalski <36817802+Raven354@users.noreply.github.com>
2020-05-09 18:07:10 +0200

    Update AnimalSeeder.java
    added sample animals

--- backend/src/main/java/com/pwr/it/app/data/seeder/AnimalSeeder.java
+++ backend/src/main/java/com/pwr/it/app/data/seeder/AnimalSeeder.java
@@ -120,6 +120,196 @@ public class AnimalSeeder {
         return initAnimal(royalPython);
     }
     
+    
+    public Animal initVeiledChameleon(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal veiledChameleon = Animal.builder()
+                .name("Nairobi")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("malnourished at young age, requires experienced handle")
+                .birthDate(getDateBackward(1))
+                .sex("female")
+                .shelterJoinDate(getDateBackward(0))
+                .user(user)
+                .build();
+        veiledChameleon.setSterilised(false);
+        treatmentHistories.forEach(treatmentHistory -> veiledChameleon.addTreatmentHistory(treatmentHistory));
+        return initAnimal(veiledChameleon);
+    }
+
+    public Animal initLeopardGecko(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal leopardGecko = Animal.builder()
+                .name("Armageddon")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("morph: classic")
+                .imageUrl("https://44536.smushcdn.com/1090522/assets/2019/12/600695.jpg?lossy=1&strip=1&webp=1")
+                .birthDate(getDateBackward(2))
+                .sex("male")
+                .shelterJoinDate(getDateBackward(1))
+                .user(user)
+                .build();
+        leopardGecko.setSterilised(false);
+        treatmentHistories.forEach(treatmentHistory -> leopardGecko.addTreatmentHistory(treatmentHistory));
+        return initAnimal(leopardGecko);
+    }
+
+    public Animal initHollandLopRabbit(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal hollandLopRabbit = Animal.builder()
+                .name("Antonio")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("small, brown/red medium fur, bit overweight - requires a specialistic diet, gets along with other rabbits")
+                .birthDate(getDateBackward(5))
+                .sex("male")
+                .shelterJoinDate(getDateBackward(1))
+                .user(user)
+                .build();
+        hollandLopRabbit.setSterilised(true);
+        treatmentHistories.forEach(treatmentHistory -> hollandLopRabbit.addTreatmentHistory(treatmentHistory));
+        return initAnimal(hollandLopRabbit);
+    }
+
+    public Animal initSulphurCrestedCockatoo(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal sulphurCrestedCockatoo = Animal.builder()
+                .name("Beaky")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("short, like on adoption sites) hand-reared, know a few words and phrases, friendly but can bite, requires an experienced owner")
+                .sex("male")
+                .shelterJoinDate(getDateBackward(1))
+                .user(user)
+                .build();
+        sulphurCrestedCockatoo.setSterilised(false);
+        treatmentHistories.forEach(treatmentHistory -> sulphurCrestedCockatoo.addTreatmentHistory(treatmentHistory));
+        return initAnimal(sulphurCrestedCockatoo);
+    }
+
+    public Animal initSugarGlider(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal sugarGlider = Animal.builder()
+                .name("Bonnie")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("bonded with Clyde, for adoption only together; friendly but doesn't like to be held or pet")
+                .birthDate(getDateBackward(2))
+                .sex("Female")
+                .shelterJoinDate(getDateBackward(0))
+                .user(user)
+                .build();
+        sugarGlider.setSterilised(true);
+        treatmentHistories.forEach(treatmentHistory -> sugarGlider.addTreatmentHistory(treatmentHistory));
+        return initAnimal(sugarGlider);
+    }
+
+    public Animal initSugarGlider(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal sugarGlider = Animal.builder()
+                .name("Clyde")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("bonded with Bonnie, for adoption only together; friendly but doesn't like to be held or pet")
+                .birthDate(getDateBackward(2))
+                .sex("male")
+                .shelterJoinDate(getDateBackward(0))
+                .user(user)
+                .build();
+        sugarGlider.setSterilised(true);
+        treatmentHistories.forEach(treatmentHistory -> sugarGlider.addTreatmentHistory(treatmentHistory));
+        return initAnimal(sugarGlider);
+    }
+
+    public Animal initLionheadRabbit(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal lionheadRabbit = Animal.builder()
+                .name("Kazik")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("medium-sized, white & brown medium fur; requires regular dental care")
+                .birthDate(getDateBackward(3))
+                .sex("female")
+                .shelterJoinDate(getDateBackward(1))
+                .user(user)
+                .build();
+        lionheadRabbit.setSterilised(true);
+        treatmentHistories.forEach(treatmentHistory -> lionheadRabbit.addTreatmentHistory(treatmentHistory));
+        return initAnimal(lionheadRabbit);
+    }
+
+    public Animal initNetherlandDwarfRabbit(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal netherlandDwarfRabbit = Animal.builder()
+                .name("Black Jack")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("small, black fur; dominant in a group, prone to attacking other rabbits")
+                .birthDate(getDateBackward(4))
+                .sex("male")
+                .shelterJoinDate(getDateBackward(1))
+                .user(user)
+                .build();
+        netherlandDwarfRabbit.setSterilised(true);
+        treatmentHistories.forEach(treatmentHistory -> netherlandDwarfRabbit.addTreatmentHistory(treatmentHistory));
+        return initAnimal(netherlandDwarfRabbit);
+    }
+
+    public Animal initFerret(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal ferret = Animal.builder()
+                .name("Beacon")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("albino; blind but brave, very friendly; hormonal implant since 21 april 2020")
+                .birthDate(getDateBackward(3))
+                .sex("male")
+                .shelterJoinDate(getDateBackward(0))
+                .user(user)
+                .build();
+        ferret.setSterilised(false);
+        treatmentHistories.forEach(treatmentHistory -> ferret.addTreatmentHistory(treatmentHistory));
+        return initAnimal(ferret);
+    }
+
+    public Animal initCockatiel(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal cockatiel = Animal.builder()
+                .name("Nico")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("white; very vocal, friendly, spent his life free-roaming around the house, feels bad in a cage")
+                .birthDate(getDateBackward(10))
+                .sex("male")
+                .shelterJoinDate(getDateBackward(1))
+                .user(user)
+                .build();
+        cockatiel.setSterilised(false);
+        treatmentHistories.forEach(treatmentHistory -> cockatiel.addTreatmentHistory(treatmentHistory));
+        return initAnimal(cockatiel);
+    }
+
+    public Animal initRussianTortoise(Species species, Race race, Set<Status> statuses, User user, Set<TreatmentHistory> treatmentHistories) {
+        Animal russianTortoise = Animal.builder()
+                .name("Thumper")
+                .species(species)
+                .race(race)
+                .statuses(statuses)
+                .description("three-legged, has a wheel in place of missing right hind leg, requires special terrarium adjustments")
+                .birthDate(getDateBackward(14))
+                .sex("female")
+                .shelterJoinDate(getDateBackward(1))
+                .user(user)
+                .build();
+        russianTortoise.setSterilised(false);
+        treatmentHistories.forEach(treatmentHistory -> russianTortoise.addTreatmentHistory(treatmentHistory));
+        return initAnimal(russianTortoise);
+    }
+
+
+    
     private Date getDateBackward(int years) {
         return new DateTime().minusYears(years).toDate();
     }
