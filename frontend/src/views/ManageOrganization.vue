f7654da240e8df805ead6c9507a4d125c2bd96cc
wojciech.spoton@comarch.com <wojciech.spoton@comarch.com>
2020-05-26 21:20:53 +0200

    Fix organization view and fix date format when null

--- frontend/src/views/ManageOrganization.vue
+++ frontend/src/views/ManageOrganization.vue
@@ -1,151 +1,159 @@
 <template>
-  <div>
-    <h1>Organization management</h1>
-    <el-row>
-      <el-col :span="11">
-        <div class="grid-content bg-purple">
+	<div>
+		<h1>Organization management</h1>
+		<div style="width: 70%; display: block; margin-left: auto; margin-right: auto; margin-bottom: 30px;">
+			<el-table v-if="isInOrganization() && !isFetching()"
+								:data="organizationTable"
+								style="width: 100%;">
+				<el-table-column
+					prop="fullName"
+					label="Name">
+				</el-table-column>
+				<el-table-column
+					prop="phoneNumber"
+					label="Phone number">
+				</el-table-column>
+				<el-table-column
+					prop="email"
+					label="Email">
+				</el-table-column>
+				<el-table-column
+					prop="address"
+					label="Address">
+				</el-table-column>
+			</el-table>
+		</div>
+		<el-row>
+			<el-col :span="11">
+				<div class="grid-content bg-purple">
+					<el-form v-if="isInOrganization() && !isFetching()" class="form-container" style="width: 90%" ref="form"
+									 :model="formReg" :rules="rulesReg"
+									 label-position="left" label-width="90%;">
+						<h2>Assign user to organization:</h2>
+						<el-form-item class="form-field" prop="pattern" label="Username" id="login-username">
 
-          <el-form v-if="isInOrganization() && !isFetching()" class="form-container" style="width: 90%" ref="form" label-position="left" label-width="90%;">
-            <h2>Your organization:</h2>
-            <div style = "position:relative; left: -20%;">
-              <h2>Name:</h2> <p style="margin-top: -40px; margin-left: 550px;">{{organization.fullName}}</p>
-            </div>
-            <div style = "position:relative; left: -20%;">
-              <h2>Phone number:</h2> <p style="margin-top: -40px; margin-left: 550px;">{{organization.phoneNumber}}</p>
-            </div>
-            <div style = "position:relative; left: -20%;">
-              <h2>Email:</h2> <p style="margin-top: -40px; margin-left: 550px;">{{organization.email}}</p>
-            </div>
-            <div style = "position:relative; left: -20%;">
-              <h2>Address:</h2> <p style="margin-top: -40px; margin-left: 550px;">{{organization.address}}</p>
-            </div>
-          </el-form>
+							<el-row>
+								<el-col :span="17">
+									<el-input placeholder="User name or email"
+														@keydown.enter.native="findUsers"
+														v-model="formReg.pattern"/>
+								</el-col>
+								<el-col :span="2">
+									<el-button type="primary" @click="findUsers" round>Search</el-button>
+								</el-col>
+							</el-row>
+						</el-form-item>
+						<el-table
+							:data="foundUser"
+							style="width: 100%">
+							<el-table-column
+								label="Name"
+								prop="fullName">
+							</el-table-column>
+							<el-table-column
+								label="Email"
+								prop="email">
+							</el-table-column>
+							<el-table-column
+								label="Phone number"
+								prop="phoneNumber">
+							</el-table-column>
+							<el-table-column
+								label="Actions">
+								<template slot-scope="scope">
+									<el-button
+										round
+										type="primary"
+										size="normal"
+										@click="addToOrganization(scope.$index)">Add
+									</el-button>
+								</template>
+							</el-table-column>
+						</el-table>
+						<br>
+					</el-form>
 
-          <el-form v-if="isInOrganization() && !isFetching()" class="form-container" style="width: 90%" ref="form" :model="formReg" :rules="rulesReg"
-                   label-position="left" label-width="90%;">
-            <h2>Assign user to organization:</h2>
-            <el-form-item class="form-field" prop="pattern" label="Username" id="login-username">
+				</div>
+			</el-col>
+			<el-col :span="2">
+				<div v-if="isInOrganization() && !isFetching()" class="grid-content bg-purple-light">
+					<img style="margin-top: 60%;" src="../assets/icons-arrow.png">
+				</div>
+			</el-col>
 
-              <el-row>
-                <el-col :span="17">
-                  <el-input placeholder="User name or email"
-                            @keydown.enter.native="findUsers"
-                            v-model="formReg.pattern"/>
-                </el-col>
-                <el-col :span="2">
-                  <el-button type="primary" @click="findUsers" round>Search</el-button>
-                </el-col>
-              </el-row>
-            </el-form-item>
-            <el-table
-              :data="foundUser"
-              style="width: 100%">
-              <el-table-column
-                label="Name"
-                prop="fullName">
-              </el-table-column>
-              <el-table-column
-                label="Email"
-                prop="email">
-              </el-table-column>
-              <el-table-column
-                label="Phone number"
-                prop="phoneNumber">
-              </el-table-column>
-              <el-table-column
-                label="Actions">
-                <template slot-scope="scope">
-                  <el-button
-                    round
-                    type="primary"
-                    size="normal"
-                    @click="addToOrganization(scope.$index)">Add
-                  </el-button>
-                </template>
-              </el-table-column>
-            </el-table>
-            <br>
-          </el-form>
+			<el-col :span="11">
+				<div class="grid-content bg-purple-light">
+					<el-form v-if="isInOrganization() && !isFetching()" class="form-container" style="width: 90%" ref="form"
+									 :model="form"
+									 :rules="rules" label-position="left" label-width="90%;">
+						<h2>Users in organization:</h2>
+						<el-table
+							:data="users"
+							style="width: 100%">
+							<el-table-column
+								label="Name"
+								prop="fullName">
+							</el-table-column>
+							<el-table-column
+								label="Email"
+								prop="email">
+							</el-table-column>
+							<el-table-column
+								label="Phone number"
+								prop="phoneNumber">
+							</el-table-column>
+							<el-table-column
+								label="Actions">
+								<template slot-scope="scope">
+									<el-button
+										round
+										type="primary"
+										style="background-color: #fb8817; border-color: #fb8817;"
+										size="normal"
+										@click="removeFromOrganization(scope.$index)">Remove
+									</el-button>
+								</template>
+							</el-table-column>
+						</el-table>
+						<br>
+					</el-form>
+				</div>
+			</el-col>
+		</el-row>
 
-        </div>
-      </el-col>
-      <el-col :span="2">
-        <div v-if="isInOrganization() && !isFetching()" class="grid-content bg-purple-light">
-          <img style="margin-top: 60%;" src="../assets/icons-arrow.png">
-        </div>
-      </el-col>
+		<el-form v-if="!isInOrganization() && !isFetching()" class="form-container" style="width: 50%" ref="form"
+						 :model="form" :rules="rules"
+						 label-position="left"
+						 label-width="140px">
+			<br>
+			<h2>Create organization:</h2>
+			<br>
+			<el-form-item class="form-field" prop="fullName" label="Organization name"
+										:error="getErrorOrganizationName()">
+				<el-input placeholder="Organization name"
+									v-model="form.fullName"/>
+			</el-form-item>
+			<el-form-item class="form-field" prop="email" label="Email" :error=getErrorForEmail()>
+				<el-input placeholder="Email"
+									v-model="form.email"/>
+			</el-form-item>
+			<el-form-item class="form-field" prop="address" label="Address" :error="getErrorForAddress()">
+				<el-input placeholder="Address"
+									v-model="form.address"/>
+			</el-form-item>
+			<el-form-item class="form-field" prop="phoneNumber" label="Phone number" :error="getErrorForPhoneNumber()">
+				<el-input placeholder="Phone number"
+									v-model="form.phoneNumber"/>
+			</el-form-item>
+			<br>
 
-      <el-col :span="11">
-        <div class="grid-content bg-purple-light">
-          <el-form v-if="isInOrganization() && !isFetching()" class="form-container" style="width: 90%" ref="form" :model="form"
-                   :rules="rules" label-position="left" label-width="90%;">
-            <h2>Users in organization:</h2>
-            <el-table
-              :data="users"
-              style="width: 100%">
-              <el-table-column
-                label="Name"
-                prop="fullName">
-              </el-table-column>
-              <el-table-column
-                label="Email"
-                prop="email">
-              </el-table-column>
-              <el-table-column
-                label="Phone number"
-                prop="phoneNumber">
-              </el-table-column>
-              <el-table-column
-                label="Actions">
-                <template slot-scope="scope">
-                  <el-button
-                    round
-                    type="primary"
-                    style="background-color: #fb8817; border-color: #fb8817;"
-                    size="normal"
-                    @click="removeFromOrganization(scope.$index)">Remove
-                  </el-button>
-                </template>
-              </el-table-column>
-            </el-table>
-            <br>
-          </el-form>
-        </div>
-      </el-col>
-    </el-row>
+			<el-button style="margin-bottom: 12px;" type="primary" @click="createOrganization()" round>Create
+			</el-button>
 
-    <el-form v-if="!isInOrganization() && !isFetching()" class="form-container" style="width: 50%" ref="form" :model="form" :rules="rules"
-             label-position="left"
-             label-width="140px">
-      <br>
-      <h2>Create organization:</h2>
-      <br>
-      <el-form-item class="form-field" prop="fullName" label="Organization name"
-                    :error="getErrorOrganizationName()">
-        <el-input placeholder="Organization name"
-                  v-model="form.fullName"/>
-      </el-form-item>
-      <el-form-item class="form-field" prop="email" label="Email" :error=getErrorForEmail()>
-        <el-input placeholder="Email"
-                  v-model="form.email"/>
-      </el-form-item>
-      <el-form-item class="form-field" prop="address" label="Address" :error="getErrorForAddress()">
-        <el-input placeholder="Address"
-                  v-model="form.address"/>
-      </el-form-item>
-      <el-form-item class="form-field" prop="phoneNumber" label="Phone number" :error="getErrorForPhoneNumber()">
-        <el-input placeholder="Phone number"
-                  v-model="form.phoneNumber"/>
-      </el-form-item>
-      <br>
+			<br>
+		</el-form>
 
-      <el-button style="margin-bottom: 12px;" type="primary" @click="createOrganization()" round>Create
-      </el-button>
-
-      <br>
-    </el-form>
-
-  </div>
+	</div>
 </template>
 
 <script>
@@ -163,6 +171,7 @@
         data() {
             return {
                 organization: Organization,
+                organizationTable: [],
                 users: [],
                 foundUser: [],
                 formReg: {
@@ -318,6 +327,8 @@
                     .then(response => {
                         this.hasOrganization = true;
                         this.organization = new Organization(response.data, this.form.phoneNumber, this.form.fullName, this.form.email, this.form.address);
+                        this.organizationTable = [];
+                        this.organizationTable.push(this.organization)
                         this.$forceUpdate();
                         this.$message({
                             type: 'success',
@@ -346,6 +357,8 @@
                 .then(response => {
                     if (response.data.id) {
                         this.organization = response.data;
+                        this.organizationTable = [];
+                        this.organizationTable.push(this.organization);
                         this.hasOrganization = true;
                     } else {
                         this.hasOrganization = false;
@@ -367,49 +380,49 @@
 </script>
 
 <style scoped>
-  >>> input {
-    border-radius: 30px;
-    text-decoration-color: black;
-    color: black;
-    border: black 1px solid;
-    width: 90%;
-    margin: 0 auto;
-    float: left;
-  }
+	>>> input {
+		border-radius: 30px;
+		text-decoration-color: black;
+		color: black;
+		border: black 1px solid;
+		width: 90%;
+		margin: 0 auto;
+		float: left;
+	}
 
-  .upload-fields {
-    /*float: left;*/
-    /*display: flex;*/
-  }
+	.upload-fields {
+		/*float: left;*/
+		/*display: flex;*/
+	}
 
-  >>> textarea {
-    border-radius: 30px;
-    text-decoration-color: black;
-    color: black;
-    border: black 1px solid;
-    width: 90%;
-    margin: 0 auto;
-    float: left;
-  }
+	>>> textarea {
+		border-radius: 30px;
+		text-decoration-color: black;
+		color: black;
+		border: black 1px solid;
+		width: 90%;
+		margin: 0 auto;
+		float: left;
+	}
 
-  .el-form-item {
-    margin: 0 auto;
-    width: 90%;
-  }
+	.el-form-item {
+		margin: 0 auto;
+		width: 90%;
+	}
 
-  >>> ::placeholder {
-    color: black;
-  }
+	>>> ::placeholder {
+		color: black;
+	}
 
-  .form-field {
-    margin-top: 16px;
-    margin-bottom: 16px;
-  }
+	.form-field {
+		margin-top: 16px;
+		margin-bottom: 16px;
+	}
 
-  .form-container {
-    margin: 0 auto;
-    width: 50%;
-    border-radius: 5px;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
-  }
+	.form-container {
+		margin: 0 auto;
+		width: 50%;
+		border-radius: 5px;
+		box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+	}
 </style>
